c3ab1750a43d0163b9c8ead371707d7a4bba822f
Hikaru Nakamura <nakamura@thedott.io>
2017-08-30 01:11:54 +0900

    I installed babel-preset-es2015 and babel-plugin-transform-decorators. I will execute "npm run eject".

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import {observable} from "mobx";
-// import {observer} from 'mobx-react'
+import {observable} from "mobx";
+import MobxFirebaseStore from 'mobx-firebase-store';
+import {createAutoSubscriber} from 'firebase-nest';
+import {observer} from 'mobx-react'
 
 @observable words = new Array();
 @observer
+
+const fbApp = firebase.initializeApp({
+  apiKey: "AIzaSyAVeQRu8yodwzbdJXBY7AcXxSEoQXYt-8I",
+  authDomain: "react-todo-6f008.firebaseapp.com",
+  databaseURL: "https://react-todo-6f008.firebaseio.com",
+  projectId: "react-todo-6f008",
+  storageBucket: "",
+  messagingSenderId: "821512865685"
+}, "react-todo");
+
+const store = new MobxFirebaseStore(firebase.database(fbApp).ref());
 
 class HelloWidget extends React.Component {
   constructor() {
